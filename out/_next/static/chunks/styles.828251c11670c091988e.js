9a0dd496d86e1092d802d809511928d71963bdb7
labertasch <labertasch@googlemail.com>
2019-05-13 16:15:54 +0200

    added static site

---
+++ out/_next/static/chunks/styles.828251c11670c091988e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["ad9d"],{FCQm:function(n,o,w){},UQhd:function(n,o,w){}}]);
---
+++ out/_next/static/webpack/styles.485705c3b7957b9991e1.hot-update.js
@@ -0,0 +1,13 @@
+webpackHotUpdate("styles",{
+
+/***/ "./scss/website.scss":
+false,
+
+/***/ 5:
+false,
+
+/***/ 6:
+false
+
+})
+//# sourceMappingURL=styles.485705c3b7957b9991e1.hot-update.js.map
