4c94419a0d922a90a4d9d27bf2962c018e95da31
labertasch <labertasch@googlemail.com>
2019-09-12 21:30:33 +0200

    bugfixing

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import "@babel/polyfill";
 import "./scss/index.scss";
-import '@fortawesome/fontawesome-free/js/all';
+import "@fortawesome/fontawesome-free/js/all";
 
 import Navbar from "./plugins/navbar";
 import Hero from "./plugins/hero";
 import CoverImage from "./plugins/coverImage";
 import ContactForm from "./plugins/ContactForm";
 
-
 class Pages {
-    constructor() {
-        ga('create', 'UA-147508305-1', 'auto');
-        //this.doAlert();
-        new Navbar("navbar");
-        new Hero("site-header");
-        new CoverImage("senol");
+  constructor() {
+    try {
+      ga("create", "UA-147508305-1", "auto");
+    } catch (e) {}
+    new Navbar("navbar");
+    new Hero("site-header");
+    new CoverImage("senol");
 
-        let forms = document.getElementsByClassName("contact-form");
-        for(let form of forms) {
-            new ContactForm(form);
-        }
+    let forms = document.getElementsByClassName("contact-form");
+    for (let form of forms) {
+      new ContactForm(form);
     }
-    
-    doAlert() {
-        alert("done");
-    }
+  }
 }
 
 //const document = window.document || {};
-document.addEventListener("DOMContentLoaded", ()=> new Pages());
+document.addEventListener("DOMContentLoaded", () =>
+  window.setTimeout(() => new Pages(), 200)
+);
