b6f0b09140c64f7bda1d3b1707094d543ef9ca49
labertasch <labertasch@googlemail.com>
2020-05-26 11:34:33 +0200

    added required fields to form

--- website/src/components/Form.js
+++ website/src/components/Form.js
@@ -14,29 +14,29 @@ function content() {
           <Col md={{ offset: 2, span: 4 }}>
             <Form.Group>
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="" required></Form.Control>
+              <Form.Control type="text" placeholder="" required name="name"></Form.Control>
             </Form.Group>
             <Form.Group>
               <Form.Label>E-Mail</Form.Label>
-              <Form.Control type="email" placeholder="" required></Form.Control>
+              <Form.Control type="email" placeholder="" required name="email"></Form.Control>
             </Form.Group>
             <Form.Group>
               <Form.Label>Company</Form.Label>
-              <Form.Control type="text" placeholder="" required></Form.Control>
+              <Form.Control type="text" placeholder="" required name="company"></Form.Control>
             </Form.Group>
             <Form.Group>
               <Form.Label>Phone</Form.Label>
-              <Form.Control type="number" placeholder="" ></Form.Control>
+              <Form.Control type="number" placeholder="" name="phone"></Form.Control>
             </Form.Group>
           </Col>
           <Col md={{ span: 4 }}>
             <Form.Group>
               <Form.Label>Subject</Form.Label>
-              <Form.Control type="text" placeholder="" required></Form.Control>
+              <Form.Control type="text" placeholder="" required name="subject"></Form.Control>
             </Form.Group>
             <Form.Group>
               <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" placeholder="" required></Form.Control>
+              <Form.Control as="textarea" placeholder="" required name="message"></Form.Control>
             </Form.Group>
             
             <Button type="submit" size="sm">Send Message</Button>
